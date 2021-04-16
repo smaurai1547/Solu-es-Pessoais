dccf980a6a96036423762271d5aebf42602b9ba2
smaurai1547 <osamisam_@hotmail.com>
2021-04-16 18:05:48 -0300

    Create Sis-Inteligente.cpp

---
+++ Sis-Inteligente.cpp
@@ -0,0 +1,29 @@
+/*
+Tentativa de elaborar um sistema inteligente
+*/
+#include <iostream>
+#include <string>
+#define endl "\n"
+using namespace std;
+
+
+int main(){
+	string entrada = "entrar";
+	cout << "Olah, diga o que deseja:" << endl;
+	while(entrada != "sair" || entrada != "Sair" || entrada != "SAIR"){
+		
+		if(){
+			asasas
+		}
+		else{
+			if(){
+				
+			}
+			else {
+				aasasds
+			}
+		}
+		cout << "Lista de Opções" << endl;
+		
+	}
+}
